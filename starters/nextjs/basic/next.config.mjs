564e5faca259ce6d001c01d1c7601bfb1d0b38df
yan04s <130055001+yan04s@users.noreply.github.com>
2025-11-14 00:44:51 +0800

    Update next.config.mjs

--- starters/nextjs/basic/next.config.mjs
+++ starters/nextjs/basic/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/__/auth/:path*',
+        destination: '/__/auth/:path*', // ensure it gets served/handled
+      },
+    ];
+  },
+};
+
 export default nextConfig;
